625d700fa540af0d425c1c84a6ee10c57337c5e8
alejopj@gmail.com <alejopj@gmail.com@f8ee08cc-79a4-4602-3726-94703eb0f909>
2013-08-06 15:51:32 +0000

    Chanded OrderState enums from char to String constants to avoid problems with Hibernate mapping.

--- src/main/java/com/google/code/imazon/model/order/util/OrderState.java
+++ src/main/java/com/google/code/imazon/model/order/util/OrderState.java
@@ -1,37 +1,21 @@
 package com.google.code.imazon.model.order.util;
 
 public enum OrderState {
-	SHOPPING_CART	('S'),
-	OUTSTANDING		('O'), // Pending of payment.
-	TBC				('T'), // To be confirmed.
-	CONFIRMED		('C'),
-	REJECTED		('R'),
-	UNDEFINED		('U');
+	SHOPPING_CART	("S"),
+	OUTSTANDING		("O"), // Pending of payment.
+	TBC				("T"), // To be confirmed.
+	CONFIRMED		("C"),
+	REJECTED		("R"),
+	UNDEFINED		("U");
 	
-	private char state;
+	private String state;
 	
-	OrderState(char state) {
+	OrderState(String state) {
 		this.state = state;
 	}
 	
-	public char toChar() {
+	@Override
+	public String toString() {
 		return state;
-	}
-	
-	public static OrderState valueOf(char state) {
-		switch (state) {
-			case ('S'):
-				return SHOPPING_CART;
-			case ('O'):
-				return OUTSTANDING;
-			case ('T'):
-				return TBC;
-			case ('C'):
-				return CONFIRMED;
-			case ('R'):
-				return REJECTED;
-			default:
-				return UNDEFINED;
-		}
 	}
 }
